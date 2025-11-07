0f734c25569233b0df1b3c5441129dd5ab2b7b83
JomnLalu <84719502+JomnLalu@users.noreply.github.com>
2025-11-07 02:10:07 -0800

    Movement Fix
    Removing physics based movement system to be less clunky while adding a global movement system.

--- Assets/Script/Control.cs
+++ Assets/Script/Control.cs
@@ -1,34 +1,41 @@
 using UnityEngine;
-
+using TMPro;
 public class Control : MonoBehaviour
 {
-    public float thrust;
-    public Rigidbody2D playerRb;
+    [Header ("Speed")]
+    public float x_thrust;
+  public float y_thrust;
+    [Header ("Workspace")]
+    private Rigidbody2D playerRb;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        playerRb = GetComponent<Rigidbody2D>();
     }
-
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.W) == true)
+        if (Input.GetKey(KeyCode.W) == true)
         {
-          playerRb.AddForce(new Vector2(playerRb.linearVelocity.x, thrust), ForceMode2D.Force);
+            //playerRb.linearVelocity = new Vector2()
+            transform.Translate(Vector2.right * y_thrust * Time.deltaTime);
         }
-        if (Input.GetKeyDown(KeyCode.A) == true)
+        if (Input.GetKey(KeyCode.A) == true)
         {
-          playerRb.AddForce(new Vector2(-thrust, playerRb.linearVelocity.y), ForceMode2D.Force);
+            //playerRb.AddForce(new Vector2(-thrust - playerRb.linearVelocity.x, playerRb.linearVelocity.y).normalized, ForceMode2D.Force);
+            transform.Translate(Vector2.up * x_thrust * Time.deltaTime);
         }
-        if (Input.GetKeyDown(KeyCode.S) == true)
+        if (Input.GetKey(KeyCode.S) == true)
         {
-          playerRb.AddForce(new Vector2(playerRb.linearVelocity.x, -thrust), ForceMode2D.Force);
-    }
-        if (Input.GetKeyDown(KeyCode.D) == true)
-        {
-          playerRb.AddForce(new Vector2(thrust, playerRb.linearVelocity.y), ForceMode2D.Force);
+            transform.Translate(Vector2.left * y_thrust * Time.deltaTime);
         }
-        
+        if (Input.GetKey(KeyCode.D) == true)
+        {
+            //playerRb.AddForce(new Vector2(thrust + playerRb.linearVelocity.x, playerRb.linearVelocity.y).normalized, ForceMode2D.Force);
+            transform.Translate(Vector2.down * x_thrust * Time.deltaTime);
+        }
+
+
+
     }
 }
